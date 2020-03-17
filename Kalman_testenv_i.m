6c606898a515e0ed9d34eb2af8f1010b0d74c8db
Daniel Bergmann <bergmann.da@gmx.de>
2020-03-17 11:01:02 +0100

    Model to Variant Subsystem, VDP and mass-spring-damper

--- Kalman_testenv_i.m
+++ Kalman_testenv_i.m
@@ -1,3 +1,16 @@
 Ts = 1e-3;
-mu = 0.2;
+
 measnoiseamp = 0.01;
+
+% Variable for choice of Variant Subsystem
+thisMode = 0;
+
+% Params VDP
+vdp.mue = 0.2;
+
+% Params Mass-Spring-Damper
+msd.k = 1;
+msd.d = 0.2;
+msd.m = 1;
+
+msd.x0 = 5;
