ac6ef80786bd5b1c89de95412d99c427f117bac9
Daniel Bergmann <bergmann.da@gmx.de>
2020-03-17 13:23:55 +0100

    added Parameter settings manually to msd_kalman_state

--- Msd_Kalman_state.m
+++ Msd_Kalman_state.m
@@ -1,6 +1,10 @@
 function [dotxhat] = Msd_Kalman_state(xhat,u)
+    msd.k = 1;
+    msd.d = 0.2;
+    msd.m = 1;
+
     k = msd.k; m = msd.m; d = msd.d;
     dotx = zeros(2,1);
-    dotx = [ x(2); -d/m*xhat(2)-k/m*xhat(1)-1/m*u];
+    dotx = [ xhat(2); -d/m*xhat(2)-k/m*xhat(1)-1/m*u];
 end
 
