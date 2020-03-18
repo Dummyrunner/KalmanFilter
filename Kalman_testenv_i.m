b3b24a5928fc06ddfbed6ded9eedee5a2f655a59
Daniel Bergmann <bergmann.da@gmx.de>
2020-03-18 10:35:45 +0100

    set System Matrices of msd in discr. and cont. within init script

--- Kalman_testenv_i.m
+++ Kalman_testenv_i.m
@@ -17,3 +17,16 @@ msd.d = 0.2;
 msd.m = 1;
 
 msd.x0 = 5;
+
+% Matrix representation of System
+k = msd.k; d=msd.d; m=msd.m;
+msd.A = [0 1; -k/m -d/m];
+msd.B = [0 1/m]';
+msd.C = [0 1];
+msd.D = 0;
+clear k d m
+
+msd.sysc = ss(msd.A,msd.B,msd.C,msd.D);
+% Obtain descrete ss-representation of sys
+msd.sysd = c2d(msd.sysc,Ts);
+[msd.Ad,msd.Bd,msd.Cd,msd.Dd] = ssdata(msd.sysd);
